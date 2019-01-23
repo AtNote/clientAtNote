2418e57d2355ce9962bdb21c2c19c6c88e53d776
jacob <1anderson2jacob@gmail.com>
2019-01-23 14:12:26 -0800

    outputParser.js is a module that formats the output of our get and delete requests

---
+++ outputParser.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function parseGet(arr) {
+  let noteArr = [];
+  let tagsArr = [];
+
+  // formats object and stick the notes and tags into arrays
+  arr.forEach(note => {
+    noteArr.push(note.note);
+    let tagString = '';
+    note.tags.forEach(tag => {
+      tagString += `@${tag}, `;
+    });
+    tagsArr.push(tagString.slice(0, tagString.length-2)); //takes comma off last command
+  });
+
+  // defining colors
+  let FgGreen = '\x1b[32m%s\x1b[0m';
+  let FgYellow = '\x1b[33m%s\x1b[0m';
+  let FgBlue = '\x1b[34m%s\x1b[0m';
+
+  // prints arrays
+  for (let i = 0; i < noteArr.length; i++) {
+    console.log(noteArr[i]);
+    console.log(FgGreen, tagsArr[i]);
+  }
+  console.log();
+}
+
+function parseDelete(numDeleted) {
+  if (numDeleted!== 1) {
+    console.log(`${numDeleted} notes deleted`);
+  } else {
+    console.log(`${numDeleted} note deleted`);
+  }
+}
+
+
+module.exports = {parseGet, parseDelete};
+
+
+
+
