bb545331d1441d4aa6e74d993b4d19c62371667e
niesssiobhan <niesssiobhan@live.com>
2019-01-23 15:16:51 -0800

    got more functionality working

--- models/edit-json.js
+++ models/edit-json.js
@@ -1,36 +1,39 @@
-
 'use strict';
 const fs = require('fs');
 
 
 //turns set into an array
-let newSet = [1234,2324,234324,123,345];
+// let newSet = [1234,2324,234324,123,345];
 
 
 //holds data that will go into file
-let toFile =[];
+function writeFile(newSet) {
+    // let toFile = [];
+    // for(let i =0; i<newSet.length; i++){
+    //     toFile.push(`"${i}":"${newSet[i]}"`);
+    //     toFile.push()
 
-for(let i =0; i<newSet.length; i++){
-    toFile.push(`"${i}":"${newSet[i]}"`);
+    // }
 
-}
+    // let nameStr = `{${toFile}}`
 
-// let nameStr = `{${toFile}}`
+    let writeContent=`'use strict';
+    let dataObject = ["${newSet}"];
+    module.exports = dataObject; `
 
-let writeContent=`'use strict';
-let dataObject ={${toFile}};
-module.exports = dataObject; `
+    // console.log(writeContent)
 
-// console.log(writeContent)
-
-function buffAppend(str){
-    var buf = Buffer.alloc(str.length);
-    buf.fill(str);
-    return str;
-}
+    function buffAppend(str){
+        var buf = Buffer.alloc(str.length);
+        buf.fill(str);
+        return str;
+    }
 
 
-fs.writeFile('./models/presistant.js', buffAppend(writeContent), function (err) {
-    if (err) throw err;
-  })
+    fs.writeFile('./models/presistant.js', buffAppend(writeContent), function (err) {
+        if (err) throw err;
+    })
 
+} //end function 
+
+module.exports = writeFile;
